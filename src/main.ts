978db409c7d95589b83a808092c649f2d5862658
Mordechai Dror <vorant94@gmail.com>
2023-07-12 07:50:24 +0300

    eslint, prettier & husky

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import express, {Request, Response} from 'express';
+import express, { type Request, type Response } from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
