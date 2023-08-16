32e40d22cd48e82a680126f67093bfebf7f9d1b6
Mordechai Dror <vorant94@gmail.com>
2023-08-16 09:59:31 +0300

    cdk in progress (need ssl connection)

--- apps/infra/src/sofash.stack.ts
+++ apps/infra/src/sofash.stack.ts
@@ -1,22 +1,39 @@
 import { RemovalPolicy, Stack, type StackProps } from 'aws-cdk-lib';
-import {
-  BlockPublicAccess,
-  Bucket,
-  BucketAccessControl,
-  ObjectOwnership,
-} from 'aws-cdk-lib/aws-s3';
 import { type Construct } from 'constructs';
+import {
+  Credentials,
+  DatabaseInstance,
+  DatabaseInstanceEngine,
+  PostgresEngineVersion,
+} from 'aws-cdk-lib/aws-rds';
+import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 
 export class SofashStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const _testBucket = new Bucket(this, `test-bucket`, {
-      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: RemovalPolicy.DESTROY,
-      accessControl: BucketAccessControl.PRIVATE,
-      objectOwnership: ObjectOwnership.BUCKET_OWNER_ENFORCED,
-      autoDeleteObjects: true,
+    const vpc = new Vpc(this, 'sofash-vpc');
+
+    const dbRootUserSecret = new Secret(this, 'sofash-db-root-user-secret', {
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: 'postgres' }),
+        generateStringKey: 'password',
+        passwordLength: 16,
+        excludePunctuation: true,
+      },
     });
+
+    const db = new DatabaseInstance(this, 'sofash-db', {
+      vpc,
+      vpcSubnets: { subnetType: SubnetType.PUBLIC },
+      engine: DatabaseInstanceEngine.postgres({
+        version: PostgresEngineVersion.VER_15_3,
+      }),
+      credentials: Credentials.fromSecret(dbRootUserSecret),
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    db.connections.allowDefaultPortFromAnyIpv4();
   }
 }
