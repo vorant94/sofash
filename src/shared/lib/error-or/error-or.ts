3f45d8aab49f46291e0bfa739238dad41bbdf36f
Mordechai Dror <vorant94@pm.me>
2024-11-16 21:47:54 +0200

    small polish

--- src/shared/lib/error-or/error-or.ts
+++ src/shared/lib/error-or/error-or.ts
@@ -1,5 +1,5 @@
 import type { Constructor } from "../constructor.ts";
 
 export type ErrorOr<T, E extends Constructor<Error>> =
-	| [undefined, T]
-	| [InstanceType<E>];
+	| [InstanceType<E>]
+	| [undefined, T];
