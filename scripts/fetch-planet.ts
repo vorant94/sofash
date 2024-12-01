bacdf7a84c67834aae61446a10557bff28e50210
Mordechai Dror <vorant94@pm.me>
2024-12-01 14:54:38 +0200

    add scripts for fetching cinema data

---
+++ scripts/fetch-planet.ts
@@ -0,0 +1,39 @@
+import console from "node:console";
+import { inspect, parseArgs } from "node:util";
+import { addDays, format } from "date-fns";
+import { z } from "zod";
+
+const { tenantId, cinemaId, date } = z
+	.object({
+		tenantId: z.coerce.number().default(10100),
+		cinemaId: z.coerce.number().default(1072),
+		date: z.coerce.date().default(addDays(new Date(), 1)),
+	})
+	.parse(
+		parseArgs({
+			options: {
+				tenantId: {
+					type: "string",
+				},
+				cinemaId: {
+					type: "string",
+				},
+				date: {
+					type: "string",
+				},
+			},
+		}).values,
+	);
+
+const response = await fetch(
+	`https://www.planetcinema.co.il/il/data-api-service/v1/quickbook/${tenantId}/film-events/in-cinema/${cinemaId}/at-date/${format(date, "yyyy-MM-dd")}`,
+);
+const json = await response.json();
+
+console.info(
+	inspect(json, {
+		colors: true,
+		depth: Number.POSITIVE_INFINITY,
+		maxArrayLength: Number.POSITIVE_INFINITY,
+	}),
+);
